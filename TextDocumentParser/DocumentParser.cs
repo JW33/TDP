ec6861b6307e42f57bf755d28a841d531120912a
Jeremy Wohltman <JW33@users.noreply.github.com>
2017-12-15 22:46:25 -0600

    Initial commit

---
+++ TextDocumentParser/DocumentParser.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextDocumentParser
+{
+    public class DocumentParser
+    {
+        public int VowelCount { get; set; }
+        public int ConsonantCount { get; set; }
+        public int LetterCount { get; set; }
+        public int WordCount { get; set; }
+        public int PalindromeCount { get; set; }
+        public int PunctuationCount { get; set; }
+        public int DigitCount { get; set; }
+
+
+        public void LoopThroughAllMethods(List<string> FileLines)
+        {
+            foreach (string s in FileLines)
+            {
+                GetVowelCount(s);
+                GetConsonantCount(s);
+                GetLetterCount();
+                GetWordCount(s);
+                GetPalindromeCount(s);
+                GetPunctuationCount(s);
+                GetDigitCount(s);
+            }
+        }
+
+        public void readInFile(string Filename, List<string> FileLines)
+        {
+            using (StreamReader r = new StreamReader(Filename))
+            {
+                string Line;
+
+                while ((Line = r.ReadLine()) != null)
+                {
+                    FileLines.Add(Line);
+                }
+            }
+        }
+
+        private void GetVowelCount(string s)
+        {
+            int loopCounter;
+            for (loopCounter = 0; loopCounter < s.Length; loopCounter++)
+            {
+                switch (s[loopCounter])
+                {
+                    case 'a':
+                    case 'A':
+                    case 'e':
+                    case 'E':
+                    case 'i':
+                    case 'I':
+                    case 'o':
+                    case 'O':
+                    case 'u':
+                    case 'U':
+                        VowelCount++;
+                        break;
+                }
+            }
+        }
+
+        private void GetConsonantCount(string s)
+        {
+            const string CONSONANTS = "qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int x = 0; x < CONSONANTS.Length; x++)
+                {
+                    if (s[i] == CONSONANTS[x])
+                    {
+                        ConsonantCount++;
+                    }
+                }
+            }
+        }
+
+        private void GetLetterCount()
+        {
+            //_letterCount = _vowelCount + _consonantCount;
+            LetterCount = VowelCount + ConsonantCount;
+        }
+
+        private void GetWordCount(string s)
+        {
+            const string NUMBERS = "1234567890";
+            const string PUNCTUATION = "~`!@#$%^&*()_-/+=[]{}|;:'\",.<>?";
+
+            string[] words = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (words.Length >= 1)
+            {
+                for (int x = 0; x < words.Length; x++)
+                {
+                    for(int y = 0; y < PUNCTUATION.Length; y++)
+                    {
+                        for( int z = 0; z < NUMBERS.Length; z++)
+                        {
+                            if (words[x] != PUNCTUATION[y].ToString() && words[x] != NUMBERS[z].ToString() && words[x] != " ")
+                            {
+                                WordCount += words.Length;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void GetPalindromeCount(string s)
+        {
+            ArrayList newArray = new ArrayList();
+
+            for (int x = s.Length - 1; x >= 0; x--)
+            {
+                newArray.Add(s[x].ToString());
+            }
+
+            StringBuilder sString = new StringBuilder();
+            foreach (string i in newArray)
+            {
+                sString.Append(i);
+            }
+
+            if (sString.ToString().Equals(s) && s != "" && s != "{" && s != "}")
+            {
+                PalindromeCount++;
+            }
+        }
+
+        private void GetPunctuationCount(string s)
+        {
+            const string PUNCTUATION = "~`!@#$%^&*()_-/+=[]{}|;:'\",.<>?";
+
+            for (int x = 0; x < s.Length; x++)
+            {
+                for (int y = 0; y < PUNCTUATION.Length; y++)
+                {
+                    if ((s[x] == PUNCTUATION[y]) && (s[x] != ' '))
+                    {
+                        PunctuationCount++;
+                    }
+                }
+            }
+        }
+
+        private void GetDigitCount(string s)
+        {
+            const string DIGITS = "0123456789";
+
+            for (int x = 0; x < s.Length; x++)
+            {
+                for (int y = 0; y < DIGITS.Length; y++)
+                {
+                    if (s[x] == DIGITS[y])
+                    {
+                        DigitCount++;
+                    }
+                }
+            }
+        }
+
+        public void GetCounts(ref int vowelCount, ref int consonantCount, ref int letterCount, ref int wordCount, ref int palindromeCount, ref int punctuationCount, ref int digitCount)
+        {
+            vowelCount = VowelCount;
+            consonantCount = ConsonantCount;
+            letterCount = LetterCount;
+            wordCount = WordCount;
+            palindromeCount = PalindromeCount;
+            punctuationCount = PunctuationCount;
+            digitCount = DigitCount;
+        }
+    }
+}
---
+++ TextDocumentParser/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextDocumentParser
+{
+    class Program
+    {
+        static int vowelCount = 0;
+        static int consonantCount = 0;
+        static int letterCount = 0;
+        static int wordCount = 0;
+        static int palindromeCount = 0;
+        static int punctuationCount = 0;
+        static int digitCount = 0;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> FileLines = new List<string>();
+                const string Filename = @"C:\Users\JW33\Documents\Misc\testing.txt";
+                //const string Filename = @"C:\Users\JW33\Documents\Misc\testingTDP.txt";
+
+                DocumentParser test = new DocumentParser();
+
+                test.readInFile(Filename, FileLines);
+
+                test.LoopThroughAllMethods(FileLines);
+
+                DisplayCounts(test);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void DisplayCounts(DocumentParser test)
+        {
+            test.GetCounts(ref vowelCount, ref consonantCount, ref letterCount, ref wordCount, ref palindromeCount, ref punctuationCount, ref digitCount);
+
+            Console.WriteLine("Number of vowels: " + vowelCount);
+            Console.WriteLine();
+
+            Console.WriteLine("Number of consonants: " + consonantCount);
+            Console.WriteLine();
+
+            Console.WriteLine("Number of letters: " + letterCount);
+            Console.WriteLine();
+
+            Console.WriteLine("Number of words: " + wordCount);
+            Console.WriteLine();
+
+            Console.WriteLine("Number of palindromes: " + palindromeCount);
+            Console.WriteLine();
+
+            Console.WriteLine("There are " + punctuationCount + " characters of punctuation");
+            Console.WriteLine();
+
+            Console.WriteLine("Number of digits: " + digitCount);
+            Console.WriteLine();
+
+            Console.WriteLine("---Program Finished---");
+
+            Console.ReadLine();
+        }
+    }
+}
